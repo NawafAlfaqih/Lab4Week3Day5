24e7ba29768fbb3daf7bd0c203bf4b7af63ad483
Nawaf Alfaqih <nawafalfaqih@outlook.com>
2025-10-30 11:19:27 +0300

    Adding 3 Objects of each class in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,42 +4,50 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("===== Circle Test =====");
-        Circle c = new Circle(12);
+        Circle c1 = new Circle(15);
+        Circle c2 = new Circle(10);
+        Circle c3 = new Circle(20);
 
-        System.out.println("Circle radius: " + c.getRadius());
-        c.setRadius(15);
-        System.out.println("Circle radius after using Mutator: " + c.getRadius());
+        System.out.println("Circle c2 radius: " + c2.getRadius());
+        c3.setRadius(12);
+        System.out.println("Circle c3 radius after using Mutator: " + c3.getRadius());
         System.out.println();
-        System.out.printf("The Area of Circle Object = %.2f \n", c.calculateArea());
-        System.out.printf("The Circumference of Circle Object = %.2f \n", c.calculateCircumference());
+        System.out.printf("The Area of Circle c1 Object = %.2f \n", c1.calculateArea());
+        System.out.printf("The Circumference of Circle c1 Object = %.2f \n", c1.calculateCircumference());
 
         System.out.println();
 
         System.out.println("===== Rectangle Test =====");
-        Rectangle rec = new Rectangle(10, 15);
-        System.out.println("Rectangle height: " + rec.getHeight());
-        System.out.println("Rectangle width: " + rec.getWidth());
-        rec.setHeight(7);
-        rec.setWidth(9);
-        System.out.println("Rectangle height after using Mutator: " + rec.getHeight());
-        System.out.println("Rectangle width after using Mutator: " + rec.getWidth());
+        Rectangle rec1 = new Rectangle(7, 9);
+        Rectangle rec2 = new Rectangle(10, 15);
+        Rectangle rec3 = new Rectangle(8, 5);
+
+        System.out.println("Rectangle rec2 height: " + rec2.getHeight());
+        System.out.println("Rectangle rec2 width: " + rec2.getWidth());
+        rec3.setHeight(12);
+        rec3.setWidth(6);
+        System.out.println("Rectangle rec3 height after using Mutator: " + rec3.getHeight());
+        System.out.println("Rectangle rec3 width after using Mutator: " + rec3.getWidth());
         System.out.println();
-        System.out.printf("The Area of Rectangle Object = %.2f \n", rec.calculateArea());
-        System.out.printf("The Perimeter of Rectangle Object = %.2f \n", rec.calculateCircumference());
+        System.out.printf("The Area of Rectangle rec1 Object = %.2f \n", rec1.calculateArea());
+        System.out.printf("The Perimeter of Rectangle rec1 Object = %.2f \n", rec1.calculateCircumference());
 
         System.out.println();
 
         System.out.println("===== Triangle Test =====");
-        Triangle tri = new Triangle(5, 6);
-        System.out.println("Triangle height: " + tri.getHeight());
-        System.out.println("Triangle base: " + tri.getBase());
-        tri.setHeight(8);
-        tri.setBase(10);
-        System.out.println("Triangle height after using Mutator: " + tri.getHeight());
-        System.out.println("Triangle width after using Mutator: " + tri.getBase());
+        Triangle tri1 = new Triangle(8, 10);
+        Triangle tri2 = new Triangle(5, 6);
+        Triangle tri3 = new Triangle(8, 9);
+
+        System.out.println("Triangle tri2 height: " + tri2.getHeight());
+        System.out.println("Triangle tri2 base: " + tri2.getBase());
+        tri3.setHeight(10);
+        tri3.setBase(12);
+        System.out.println("Triangle tri3 height after using Mutator: " + tri3.getHeight());
+        System.out.println("Triangle tri3 width after using Mutator: " + tri3.getBase());
         System.out.println();
-        System.out.printf("The Area of Triangle Object = %.2f \n", tri.calculateArea());
-        System.out.printf("The Perimeter of Triangle Object = %.2f \n", tri.calculateCircumference());
+        System.out.printf("The Area of Triangle tri1 Object = %.2f \n", tri1.calculateArea());
+        System.out.printf("The Perimeter of Triangle tri1 Object = %.2f \n", tri1.calculateCircumference());
 
     }
 }
